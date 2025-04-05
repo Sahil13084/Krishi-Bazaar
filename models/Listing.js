30ca4e17de4a36219cc8e5e8e168b13517e6ac1a
Sahil13084 <sahil1308yadav@gmail.com>
2025-04-05 09:10:02 +0530

    Update

--- models/Listing.js
+++ models/Listing.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
 
 const listingSchema = new mongoose.Schema( { name: String, product: String, price: Number, phone: String, description: String, state: String, imageUrl: String }, { timestamps: true }); // ← adds createdAt and updatedAt automatically );
+
 module.exports = mongoose.model("Listing", listingSchema);
